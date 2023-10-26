1172a253018bd89b3fd8162bc3d38d5ea9a7151c
YoonMin <dbsals999@naver.com>
2023-10-26 20:13:57 +0900

    게시물 리스팅, 게시물 추가 기능 구현

--- src/main/java/com/ym/jdbc/App.java
+++ src/main/java/com/ym/jdbc/App.java
@@ -2,9 +2,18 @@ package com.ym.jdbc;
 
 import com.ym.jdbc.container.Container;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
+  List<Article> articles;
+  int articleLastId;
+
+  public App() {
+    articles = new ArrayList<>();
+    articleLastId = 0;
+  }
 
   public void run() {
     Scanner sc = Container.scanner;
@@ -13,8 +22,41 @@ public class App {
       System.out.printf("명령어) ");
       String cmd = sc.nextLine();
 
-      if(cmd.equals("/usr/article/list")) {
+      if(cmd.equals("/usr/article/write")) {
+        System.out.println("== 게시물 등록 ==");
+
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+
+        System.out.printf("내용 : ");
+        String content = sc.nextLine();
+        int id = ++articleLastId;
+
+        Article article = new Article(id, title, content);
+        articles.add(article);
+
+        System.out.printf("%d번 게시물을 작성하였습니다.\n", article.id);
+      }
+      else if(cmd.equals("/usr/article/list")) {
         System.out.println("== 게시물 리스트 ==");
+
+        if(articles.isEmpty()) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        System.out.println("번호 / 제목");
+
+        /*
+        for(Article article : articles) {
+          System.out.printf("%d / %s\n", article.id, article.title);
+        }
+        */
+
+        for(int i = articles.size() - 1; i >= 0; i--) {
+          Article article = articles.get(i);
+          System.out.printf("%d / %s\n", article.id, article.title);
+        }
       }
       else if(cmd.equals("exit")) {
         System.out.println("프로그램 종료");
