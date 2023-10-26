b9cee32fe795fe1b93b15203eaf211960decb030
YoonMin <dbsals999@naver.com>
2023-10-26 20:53:18 +0900

    Select 테스트

--- src/main/java/com/ym/jdbc/Article.java
+++ src/main/java/com/ym/jdbc/Article.java
@@ -8,8 +8,18 @@ import java.util.Scanner;
 
 public class Article {
   int id;
+  String regDate;
+  String updateDate;
   String title;
   String content;
+
+  public Article(int id, String regDate, String updateDate, String title, String content) {
+    this.id = id;
+    this.regDate = regDate;
+    this.updateDate = updateDate;
+    this.title = title;
+    this.content = content;
+  }
 
   public Article(int id, String title, String content) {
     this.id = id;
@@ -21,6 +31,8 @@ public class Article {
   public String toString() {
     return "Article{" +
         "id=" + id +
+        ", regDate='" + regDate + '\'' +
+        ", updateDate='" + updateDate + '\'' +
         ", title='" + title + '\'' +
         ", content='" + content + '\'' +
         '}';
